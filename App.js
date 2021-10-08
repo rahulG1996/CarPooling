e7709f3a9c10f37feac96fabe6eac207e4e45975
rahul gupta <rahulgupta@rahuls-MacBook-Air.local>
2021-10-08 17:37:26 +0530

    splash screen done on both android and ios

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return null;
+};
+
+export default App;
---
+++ android/app/src/main/java/com/carpooling_shareit/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.carpooling_shareit; // ← Make sure that is your package name
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity; // ← For RN <= 0.59
+
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
